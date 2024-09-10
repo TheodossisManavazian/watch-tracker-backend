5f93b0093047710e3c2ce153e92a40223708d836
Theodossis Manavazian <theomanavazian@gmail.com>
2024-09-10 01:38:58 -0400

    Update caliber.py

--- watch_service/models/caliber.py
+++ watch_service/models/caliber.py
@@ -5,8 +5,9 @@ class Caliber(BaseModel):
     caliber: str
     brand: str
     movement: str
-    power_reserve: str
-    qty_jewels: str
+    power_reserve: str | None
+    qty_jewels: str | None
+    frequency: str | None
 
     def __str__(self) -> str:
         return "\n".join("%s: %s" % item for item in vars(self).items())
